08b8e4131e4e9b80733627304afa308f335c7458
Kalita Mikhail <mkalita1991@gmail.com>
2018-10-05 18:09:56 +0300

    7 from 17

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+	
+  function Eiler(pairs) {
+    let eiler = 1;
+  
+    for ( let i=0; i < pairs.length; i++ ) 
+      eiler = eiler * ( Math.pow(pairs[i][0], pairs[i][1])
+                      - Math.pow(pairs[i][0], pairs[i][1] - 1) );
+					  
+    return eiler;
+  }  
+  
+
+  function Number(pairs) {
+    let multiply = 1;
+  
+    for ( let i=0; i < pairs.length; i++ ) 
+      multiply = multiply * Math.pow(pairs[i][0], pairs[i][1]);
+                      
+    return multiply;
+  }  
+  
+
+  if (s[0] == '1') return Eiler(pairs);
+    else return Number(pairs) - Eiler(pairs);
+  
 }
